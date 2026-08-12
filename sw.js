26e43ce72577c1894f58d0aa509a665ff585a03b
mapper-web <316004478+mapper-web@users.noreply.github.com>
2026-08-11 21:28:09 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='2026-08-12-d5184aff55';
+const V='2026-08-12-21e14936b9';
 const ASSETS=["./", "index.html", "data/bundle.js", "manifest.webmanifest", "icons/icon-192.png", "icons/icon-512.png"];
 
 self.addEventListener('install', e => {
